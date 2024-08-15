fd04327a45b655f6662bec3458be59e40cd3efe0
fususa <sujira.sa@kkumail.com>
2024-08-15 17:33:01 +0700

    firstlaravel

---
+++ resources/views/myshop.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>เกี่ยวกับร้าน IT SHOP </h1>
+    <ul>
+        <li>ที่อยู่ : {{$address}}</li>
+        <li>เบอร์ : {{$mobilephone}}</li>
+        <li>ที่ตั้ง : {{$location}}</li>
+    </ul>
+</body>
+</html>
