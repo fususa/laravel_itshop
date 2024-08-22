6cae95fc69a6eb5765700996c034c3928dd70b10
fususa <sujira.sa@kkumail.com>
2024-08-22 17:10:43 +0700

    first app

--- resources/views/myshop.blade.php
+++ resources/views/myshop.blade.php
@@ -1,16 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>เกี่ยวกับร้าน IT SHOP </h1>
-    <ul>
-        <li>ที่อยู่ : {{$address}}</li>
-        <li>เบอร์ : {{$mobilephone}}</li>
-        <li>ที่ตั้ง : {{$location}}</li>
-    </ul>
-</body>
-</html>
+@extends('layouts.myapp')
+
+@section('content')
+    <div class = "row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h2>เกี่ยวกับร้าน IT SHOP</h2>
+                </div>
+                <div class="card-body">
+                    <ul>
+                        <li>ที่อยู่ : {{$address}}</li>
+                        <li>เบอร์ : {{$mobilephone}}</li>
+                        <li>ที่ตั้ง : {{$location}}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
