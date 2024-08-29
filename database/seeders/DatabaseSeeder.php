6c3d969f24e213329081afe006b99b41e4b70fb9
fususa <sujira.sa@kkumail.com>
2024-08-29 17:04:50 +0700

    listshop

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,38 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
+    public function run(): void{
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    //     User::factory()->create([
+    //         'name' => 'fususa',
+    //         'email' => 'fuangsujira@gmail.com',
+    //         'password' => Hash::make('fususa')
+    //     ]);
+    // }
+    /**
+     * Seed the application's database.
+     */
+    // public function run(): void
+    // {
+    //     // User::factory(10)->create();
+
+    //     User::factory()->create([
+    //         'name' => 'Test User',
+    //         'email' => 'test@example.com',
+    //     ]);
+    // }
+    $this->call([
+        ProductTypesTableSeeder::class,
+        ProductsTableSeeder::class,
+    ]);
     }
 }
---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            ['name' => 'ชาเขียว',
+            'cost' => 5000,
+            'price' => 50 ,
+            'qty' => 20,
+            'image_path' => '',
+            'image_url' => 'https://th.bing.com/th/id/OIP.T25xJ9no0udJGsk4z5ri5gHaHa?rs=1&pid=ImgDetMain',
+            'product_type_id' => 2],
+
+            ['name' => 'โกโก้',
+            'cost' => 10000,
+            'price' => 60 ,
+            'qty' => 50,
+            'image_path' => '',
+            'image_url' => 'https://yalamarketplace.com/upload/1624449872880.jpg',
+            'product_type_id' => 2],
+            ];
+            DB::table('products')->insert($data);
+    }
+}
+
